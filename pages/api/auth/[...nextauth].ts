16ed8b274ab5777d1ab6a39237ca89bfeba6d0ca
notoli <oliglockler@googlemail.com>
2025-10-02 12:09:53 +0100

    updated nextauth

--- pages/api/auth/[...nextauth].ts
+++ pages/api/auth/[...nextauth].ts
@@ -4,8 +4,8 @@ import SpotifyProvider from "next-auth/providers/spotify";
 export default NextAuth({
   providers: [
     SpotifyProvider({
-      clientId: process.env.26327cd7979a48ad817b052bc3de623c!,
-      clientSecret: process.env.efaafca737ea4d4d8fff1933347d4327!,
+      clientId: process.env.SPOTIFY_CLIENT_ID!,
+      clientSecret: process.env.SPOTIFY_CLIENT_SECRET!,
       authorization:
         "https://accounts.spotify.com/authorize?scope=user-read-email",
     }),
