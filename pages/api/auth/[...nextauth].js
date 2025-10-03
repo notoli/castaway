2c25556fe9db6bb62aeee13dfad1a6d92faf2086
notoli <oliglockler@googlemail.com>
2025-10-03 13:15:41 +0100

    updates js inside nextauth

--- pages/api/auth/[...nextauth].js
+++ pages/api/auth/[...nextauth].js
@@ -4,11 +4,26 @@ import SpotifyProvider from "next-auth/providers/spotify";
 export default NextAuth({
   providers: [
     SpotifyProvider({
-      clientId: process.env.SPOTIFY_CLIENT_ID!,
-      clientSecret: process.env.SPOTIFY_CLIENT_SECRET!,
-      authorization: "https://accounts.spotify.com/authorize?scope=user-read-email",
+      clientId: process.env.SPOTIFY_CLIENT_ID,
+      clientSecret: process.env.SPOTIFY_CLIENT_SECRET,
+      authorization:
+        "https://accounts.spotify.com/authorize?scope=user-read-email",
     }),
   ],
   secret: process.env.NEXTAUTH_SECRET,
-  trustHost: true, // This forces NextAuth to use NEXTAUTH_URL instead of guessing
+  trustHost: true, // ensures NEXTAUTH_URL is respected on Vercel
+  callbacks: {
+    async jwt({ token, account }) {
+      if (account) {
+        token.accessToken = account.access_token;
+        token.refreshToken = account.refresh_token;
+      }
+      return token;
+    },
+    async session({ session, token }) {
+      session.accessToken = token.accessToken;
+      session.refreshToken = token.refreshToken;
+      return session;
+    },
+  },
 });
