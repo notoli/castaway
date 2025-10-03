1caee2cc32677068c31ec444bbc2dcbd950659b2
notoli <oliglockler@googlemail.com>
2025-10-03 16:22:39 +0100

    replaced login style to match index

--- pages/login.js
+++ pages/login.js
@@ -3,16 +3,14 @@ import styles from "../styles/Login.module.css";
 
 export default function Login() {
   return (
-    <div className={styles.loginContainer}>
-      <h1 className={styles.loginTitle}>Desert Island Albums</h1>
-      <p className={styles.loginDescription}>
+    <div className={styles.container}>
+      <h1 className={styles.title}>Desert Island Albums</h1>
+      <p className={styles.description}>
         Save your top 5 albums and create your personal Desert Island playlist. Sign in with Spotify to get started.
       </p>
       <button
         className={styles.loginButton}
-        onClick={() =>
-          signIn("spotify", { callbackUrl: "/" })
-        }
+        onClick={() => signIn("spotify", { callbackUrl: "/" })}
       >
         Sign in with Spotify
       </button>
