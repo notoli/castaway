0bcaaeb8b9dda4eff5eb6835647adbd4503002b5
notoli <oliglockler@googlemail.com>
2025-10-03 13:37:31 +0100

    updated nextauth to include supabase

--- pages/api/auth/[...nextauth].js
+++ pages/api/auth/[...nextauth].js
@@ -12,18 +12,21 @@ export default NextAuth({
   ],
   secret: process.env.NEXTAUTH_SECRET,
   trustHost: true, // ensures NEXTAUTH_URL is respected on Vercel
-  callbacks: {
-    async jwt({ token, account }) {
-      if (account) {
-        token.accessToken = account.access_token;
-        token.refreshToken = account.refresh_token;
-      }
-      return token;
-    },
-    async session({ session, token }) {
-      session.accessToken = token.accessToken;
-      session.refreshToken = token.refreshToken;
-      return session;
-    },
+callbacks: {
+  async jwt({ token, account, user }) {
+    if (account) {
+      token.accessToken = account.access_token;
+      token.refreshToken = account.refresh_token;
+      token.id = user.id; // add the user ID to the token
+    }
+    return token;
   },
+  async session({ session, token }) {
+    session.accessToken = token.accessToken;
+    session.refreshToken = token.refreshToken;
+    session.user.id = token.id; // now session.user.id exists
+    return session;
+  },
+},
+
 });
